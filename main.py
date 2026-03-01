62a2f4ab2915b78f7416d78e66822e204cbd9547
salohiddin27 <salohiddinnurbayev25@gmail.com>
2026-03-01 13:41:46 +0500

    New commands

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ def get_main_menu():
     ikb.row(
         InlineKeyboardButton(text="Calculator 🔢", callback_data="calcula_"),
         InlineKeyboardButton(text="Languages 🇺🇿", callback_data="language_"),
-        InlineKeyboardButton(text="English_dictionary 📚", callback_data="dictionary_")
+        InlineKeyboardButton(text="English_dictionary 📚", callback_data="dictionary_"),
+        InlineKeyboardButton(text="Games 🚗", callback_data="game_")
     )
     ikb.adjust(2)
     return ikb.as_markup()
@@ -51,6 +52,302 @@ async def commands_start(message: Message, state: FSMContext):
         text="O'zizga kerakli tugmani tanlang!",
         reply_markup=get_main_menu()
     )
+
+
+@dp.callback_query(F.data == 'game_')
+async def show_all_cars(callback: CallbackQuery):
+    await callback.message.delete()
+    ikb = InlineKeyboardBuilder()
+    ikb.row(
+        InlineKeyboardButton(text="Lightweight car 🚘", callback_data="ligt_cars"),
+        InlineKeyboardButton(text="Truck 🚛", callback_data="track_"),
+        InlineKeyboardButton(text="Motorbikes 🏍️", callback_data="motor_bice_"),
+        InlineKeyboardButton(text="Archery game 🏹", callback_data="kamon_"),
+        InlineKeyboardButton(text="Back 🔙", callback_data="back_"),
+    )
+    ikb.adjust(2)
+    await callback.message.answer(f"O'zizga yoqgan o'yini tanlang! ", reply_markup=ikb.as_markup())
+
+
+@dp.callback_query(F.data == 'back_')
+async def get_to_main(callback: CallbackQuery, state: FSMContext):
+    await callback.message.delete()
+    await state.clear()
+    await callback.message.answer(text="Asosiy menyuga qaytingiz. O'zizga kerakli tugmani bosing:",
+                                  reply_markup=get_main_menu())
+
+
+@dp.callback_query(F.data == 'kamon_')
+async def show_shot(callback: CallbackQuery):
+    await callback.message.delete()
+    ikb = InlineKeyboardBuilder()
+    ikb.row(InlineKeyboardButton(text="Stik War ⚔️", callback_data="war_"),
+            InlineKeyboardButton(text="The Archers 2 🏹", callback_data="archers_"),
+            InlineKeyboardButton(text="Ninja's Creed: 3D 🥷", callback_data="ninja_"),
+            InlineKeyboardButton(text="Angry Birds 🐦", callback_data="birds_"),
+            InlineKeyboardButton(text="Thetan Immortal 🦸", callback_data="immortal_"),
+            InlineKeyboardButton(text="Archer Shooter 🏹🎯", callback_data="shooter_"),
+            InlineKeyboardButton(text="Back 🔙", callback_data="back_"),
+
+            )
+    ikb.adjust(2)
+    await callback.message.answer(f"O'zizga yoqgan o'yini tanlang! ", reply_markup=ikb.as_markup())
+
+
+@dp.callback_query(F.data == 'war_')
+async def show_war(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.maxgames.stickwarlegacy"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'archers_')
+async def show_war(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.byv.TheArchers2"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'ninja_')
+async def show_war(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=ninja.creed.sniper.real3d.action.free.android"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'birds_')
+async def show_war(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.rovio.angrybirdsfriends"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'immortal_')
+async def show_war(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.wolffun.thetanimmortal"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'shooter_')
+async def show_war(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.action_games.archer.shooter.attack"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'back_')
+async def get_to_main(callback: CallbackQuery, state: FSMContext):
+    await callback.message.delete()
+    await state.clear()
+    await callback.message.answer(text="Asosiy menyuga qaytingiz. O'zizga kerakli tugmani bosing:",
+                                  reply_markup=get_main_menu())
+
+
+@dp.callback_query(F.data == 'motor_bice_')
+async def show_motor_bice(callback: CallbackQuery):
+    ikb = InlineKeyboardBuilder()
+    ikb.row(InlineKeyboardButton(text="Traffic Rider 🏍️", callback_data="motor_bike_"),
+            InlineKeyboardButton(text="Moto Bike Stunt 🏍️💨", callback_data="bike_stunt_"),
+            InlineKeyboardButton(text="Bike Racing 3D 🚴🏻‍♂️", callback_data="bike_"),
+            InlineKeyboardButton(text="MRM Bike Racing 🏍️🏁", callback_data="race_"),
+            InlineKeyboardButton(text="Mountain Bike 🚵", callback_data="mountain_bike_"),
+            InlineKeyboardButton(text="Bike Blast 🚴🏻‍♂️", callback_data="blast_"),
+            InlineKeyboardButton(text="Back 🔙", callback_data="back_"),
+
+            )
+    ikb.adjust(2)
+    await callback.message.answer("Yoqgan O'yini tanlang! ", reply_markup=ikb.as_markup())
+
+
+@dp.callback_query(F.data == 'motor_bike_')
+async def show_moto(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.skgames.trafficrider"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'bike_stunt_')
+async def show_moto(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.gt.moto.bike.wheelie.stunt.race.game"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'bike_')
+async def show_moto(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.kn.bikestunt3.racing.driving.games"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'race_')
+async def show_moto(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.offline.racing.motorcyclegame.motomax.bikerace.bike.games"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'mountain_bike_')
+async def show_moto(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.PixelJoy.MountainBikeXtreme"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'blast_')
+async def show_moto(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.aceviral.bmxblast"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'back_')
+async def get_to_main(callback: CallbackQuery, state: FSMContext):
+    await callback.message.delete()
+    await state.clear()
+    await callback.message.answer(text="Asosiy menyuga qaytingiz. O'zizga kerakli tugmani bosing:",
+                                  reply_markup=get_main_menu())
+
+
+@dp.callback_query(F.data == 'track_')
+async def show_truck(callback: CallbackQuery):
+    ikb = InlineKeyboardBuilder()
+    ikb.row(InlineKeyboardButton(text="Truck Simulator 🚛", callback_data="simulator_t"),
+            InlineKeyboardButton(text="Truckers of Europe 3 🚚", callback_data="europa_"),
+            InlineKeyboardButton(text="Truck Driving School 🚚", callback_data="school_"),
+            InlineKeyboardButton(text="Frontline WW2 ⛓️", callback_data="ww2_"),
+            InlineKeyboardButton(text="Offoard Truck 🚚", callback_data="offfard_"),
+            InlineKeyboardButton(text="Truck Star 🚛", callback_data="star_"),
+            InlineKeyboardButton(text="Back 🔙", callback_data="back_")
+
+            )
+    ikb.adjust(2)
+    await callback.message.answer("O'zizga yoqgan o'yini tanlang! ", reply_markup=ikb.as_markup())
+
+
+@dp.callback_query(F.data == 'simulator_t')
+async def get_truc(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.zuuks.truck.simulator.ultimate"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'europa_')
+async def get_truc(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.WandaSoftware.TruckersofEurope3"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'school_')
+async def get_truc(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.bettergames.truck.drivingschool.simulator"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'ww2_')
+async def get_truc(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=truck.simulator.climb.frontline"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'offfard_')
+async def get_truc(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.pw.trucksimulatoreuro.offroadcargo"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'star_')
+async def get_truc(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.vm3.global"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'back_')
+async def get_to_main(callback: CallbackQuery, state: FSMContext):
+    await callback.message.delete()
+    await state.clear()
+    await callback.message.answer(text="Asosiy menyuga qaytingiz. O'zizga kerakli tugmani bosing:",
+                                  reply_markup=get_main_menu())
+    await callback.answer()
+
+
+@dp.callback_query(F.data == 'ligt_cars')
+async def show_car(callback: CallbackQuery):
+    ikb = InlineKeyboardBuilder()
+    ikb.row(
+        InlineKeyboardButton(text="Payback 2 🚘", callback_data="payback_"),
+        InlineKeyboardButton(text="Hill Climb racing 🛻", callback_data="hill_climb_"),
+        InlineKeyboardButton(text="Dr.Driving 🚘", callback_data="driving_"),
+        InlineKeyboardButton(text="Traffic Racer 🏎️", callback_data="racer_"),
+        InlineKeyboardButton(text="Mountain Climb 🚘", callback_data="mountain_climb_"),
+        InlineKeyboardButton(text="Stock Car Racing 🏎️", callback_data="stock_car_"),
+        InlineKeyboardButton(text="Extreme Car 🏎", callback_data="extreme_"),
+        InlineKeyboardButton(text="Traffic racer 🏎️", callback_data="racer_"),
+        InlineKeyboardButton(text="MIMX Hill Dash 🛻", callback_data="dash_"),
+        InlineKeyboardButton(text="Car Crash Royale ⛔", callback_data="crash_"),
+        InlineKeyboardButton(text="Back 🔙", callback_data="back_"),
+
+    )
+    ikb.adjust(2)
+    await callback.message.edit_text("O'zizga yoqgan o'yini tanlang!", reply_markup=ikb.as_markup())
+
+
+@dp.callback_query(F.data == "payback_")
+async def show_payback(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=net.apex_designs.payback2"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'hill_climb_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.fingersoft.hillclimb"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'driving_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.ansangha.drdriving"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'racer_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.skgames.trafficracer"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'mountain_climb_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.dagtirmanma.oyunu"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'stock_car_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.minicades.stockcars"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'extreme_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.aim.racing"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'racer_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.skgames.trafficracer"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'dash_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.hutchgames.hilldash2"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == 'crash_')
+async def show_hill_climb(callback: CallbackQuery):
+    url = "https://play.google.com/store/apps/details?id=com.FailGames.RoyalCarDestroy"
+    await callback.message.answer(f"Siz so'ragan mashina umid qilamanki bu o'yin sizga yoqadi 😊\n\n{url}")
+
+
+@dp.callback_query(F.data == "back_")
+async def get_to_main(callback: CallbackQuery, state: FSMContext):
+    await callback.message.delete()
+    await state.clear()
+
+    await callback.message.answer(text="Asosiy menyuga qaytingiz. O'zizga kerakli tugmani bosing:",
+                                  reply_markup=get_main_menu())
+    await callback.answer()
 
 
 @dp.callback_query(F.data == 'dictionary_')
